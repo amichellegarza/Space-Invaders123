cc19666cfdad4022866b1cde20b12e0254b9a22c
amichellegarza <amichelle.garza@gmail.com>
2024-06-17 15:09:24 -0500

    Drew image and text
    Drew image and text per assignment instructions. Added rotation to square so it presents as a square rather than a diamond.

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     //Defined video mode (dimensions)
-    sf::VideoMode videoMode = sf::VideoMode( 800,600 );
+    sf::VideoMode videoMode = sf::VideoMode( 1000,1000 );
 
     //Create a window object with specific dimensions and a title
     sf::RenderWindow window( videoMode, "My SFML Window" );
@@ -20,22 +20,42 @@ int main()
         window.clear(sf::Color::Black);
 
         // Drawing a green circle 
-        sf::CircleShape circle(60); // The radius of this circle is set to 50
+            sf::CircleShape circle(30); // The radius of this circle is set to 50
             circle.setFillColor(sf::Color::Green); //The color of this circle is red
-            circle.setPosition(200, 150); //Set position on screen
+            circle.setPosition(900, 100); //Set position on screen
             window.draw(circle);
 
         // Drawing a red square
-        sf::CircleShape square(80.f, 4);
+            sf::CircleShape square(80.f, 4);
             square.setFillColor(sf::Color::Red);
-            square.setPosition(20, 20);
+            square.setPosition(300, 20);
+            square.setRotation(45);
             window.draw(square);
 
         // Drawing a blue triangle
             sf::CircleShape triangle(40.f, 3);
             triangle.setFillColor(sf::Color::Blue);
-            triangle.setPosition(100, 200);
+            triangle.setPosition(200, 200);
             window.draw(triangle);
+
+        // Drawing the Outscal texture
+            sf::Texture outscal_texture;
+            outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
+
+        // Drawing the Outscal sprite
+            sf::Sprite outscal_sprite;
+            outscal_sprite.setTexture(outscal_texture);
+            outscal_sprite.setPosition(400, 100);
+            outscal_sprite.setRotation(45);
+            outscal_sprite.setScale(0.5, 0.5);
+            window.draw(outscal_sprite);
+
+        // Drawing font
+            sf::Font font;
+            font.loadFromFile("assets/fonts/OpenSans.ttf");
+            sf::Text text("SFML is awesome!", font, 50);
+            text.setFillColor(sf::Color::White);
+            window.draw(text);
 
         //Display whatever you draw
         window.display();
