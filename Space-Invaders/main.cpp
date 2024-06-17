c209d23f64d560a2e14349bd81078cb2981274f9
amichellegarza <amichelle.garza@gmail.com>
2024-06-17 14:36:49 -0500

    Drew some shapes in previously created window!

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -17,7 +17,25 @@ int main()
                 window.close();
         }
         // Clear the window
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Black);
+
+        // Drawing a green circle 
+        sf::CircleShape circle(60); // The radius of this circle is set to 50
+            circle.setFillColor(sf::Color::Green); //The color of this circle is red
+            circle.setPosition(200, 150); //Set position on screen
+            window.draw(circle);
+
+        // Drawing a red square
+        sf::CircleShape square(80.f, 4);
+            square.setFillColor(sf::Color::Red);
+            square.setPosition(20, 20);
+            window.draw(square);
+
+        // Drawing a blue triangle
+            sf::CircleShape triangle(40.f, 3);
+            triangle.setFillColor(sf::Color::Blue);
+            triangle.setPosition(100, 200);
+            window.draw(triangle);
 
         //Display whatever you draw
         window.display();
